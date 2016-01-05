99a82cf27547bf195d88914f45d0f59e53ad2eaf
stefano <stefanos@atlas.elphodomain.local>
2016-01-05 13:05:55 +0200

    select primary key

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 query builder for postgres
 A little library for constructing raw sql queries.
 
-# example 1
+# examples
+### example 1
 SELECT * FROM PROPERTY
 ```javascript
 var q = qb.select('*')+qb.from('property');
 ```
-
-# example 2
+### example 2
 SELECT * FROM PROPERTY LEFT JOIN TENANT ON PROPERTY.TENANT_ID = TENANT.TENANT_ID
 ```javascript
 var q = qb.select('*') + qb.from('property')+qb.leftjoin('property','tenant', 'tenant_id');
+```
+### example 3
+UPDATE TENANT SET at1 = 'val1', at2 = 'val2' WHERE tenant_id = 27
+```javascript
+var attributes = ["at1", "at2"];
+var values = ["'val1'", "'val2'"];
+var keyexpression = "tenant_id = 27";
+var q = qb.update("tenant", attributes, values)+qb.where(keyexpression);
 ```
 
 # methods
@@ -19,23 +27,10 @@ var q = qb.select('*') + qb.from('property')+qb.leftjoin('property','tenant', 't
 var Constructor = require('tougo-query-builder');
 ```
 
-## var instance = new Constructor()
-describe constructor
-
-## instance.method()
-describe methods
-
 # install
 ```bash
 npm install tougo-query-builder
 ```
 
-# test
-```bash
-npm test
-```
-
 # license
 ISC
-
-
--- query-builder.js
+++ query-builder.js
@@ -3,19 +3,19 @@
  */
 module.exports = {
     select: function (what) {
-        return " select " + what;
+        return " SELECT " + what;
     },
     from: function (table) {
-        return " from " + table;
+        return " FROM " + table;
     },
     innerjoin: function (table1, table2, attribute) {
-        return " INNER JOIN " + table2 + " ON " + table1 + "." + attribute + " = " + table2 + "." + attribute + " "
+        return " INNER JOIN " + table2 + " ON " + table1 + "." + attribute + " = " + table2 + "." + attribute;
     },
     leftjoin: function (table1, table2, attribute) {
-        return " LEFT JOIN " + table2 + " ON " + table1 + "." + attribute + " = " + table2 + "." + attribute + " "
+        return " LEFT JOIN " + table2 + " ON " + table1 + "." + attribute + " = " + table2 + "." + attribute;
     },
     where: function (expression) {
-        return " where " + expression;
+        return " WHERE " + expression;
     },
     insertinto: function (table, attributes, values) {
         if (attributes.length === values.length) {
@@ -31,18 +31,19 @@ module.exports = {
     },
     update: function (table, attributes, values) {
         if (attributes.length === values.length) {
-            var sql = " UPDATE " + table + " SET "
+            var sql = " UPDATE " + table + " SET ";
             var i;
             var set = [];
-            for (i = 0; i < attributes.length; i++) {
+            for (i = 0; i < attributes.length; i+=1) {
                 set.push(attributes[i] + "=" + values[i]);
             }
-            return sql += set.join();
+            sql += set.join();
+            return sql;
         }
         return "attribute, values length missmatch";
     },
-    delete: function (what) {
-        return " delete ";
+    delete: function () {
+        return " DELETE ";
     },
     groupby: function(attributes) {
         var sql =" GROUP BY(";
@@ -67,11 +68,16 @@ module.exports = {
       var orderby = [];
       if (attributes.length === ordertype.length) {
         var i;
-        for (i = 0; i < attributes.length; i++) {
+        for (i = 0; i < attributes.length; i+=1) {
               orderby.push(attributes[i] + " " + ordertype[i]);
             }
-            return sql += orderby.join();
+            sql += orderby.join();
+            return sql;
         }
         return "attribute, ordertype length missmatch";
+    },
+    primarykey: function(table) {
+      var sql = "SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type FROM   pg_index i JOIN   pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey) WHERE  i.indrelid = '"+ table+"'::regclass AND i.indisprimary";
+      return sql;
     }
 }
